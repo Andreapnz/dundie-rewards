80406bc030a4bccd5897287938f27f8724ccc4d5
Andréa Correia <andrea_pnz@hotmail.com>
2025-07-13 17:37:06 -0300

    turned to installable binary #9

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+
+setup(
+    name="dundie",
+    version="0.1.0",
+    description="Reward Point System for Dundie Miffin",
+    author="Andrea Correia",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "dundie = dundie.__main__:main"
+        ]
+    },
+)
