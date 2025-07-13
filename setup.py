1dd9b4fc528a78cdd294667b50da9345adbe3eaa
Andrea Correia <andrea_pnz@hotmail.com>
2025-07-12 23:17:43 -0300

    added gitignore #8 (#11)
    * added gitignore #8
    
    * Set up initial organization and metadata
    
    - Add setup.py to turn installable
    - Add __main__.py for executable
    - Add __init__ for package
    
    closes #8
    
    
    closes #8

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+
+setup(
+    name="dundie",
+    version="0.1.0",
+    description="Reward Point System for Dundie Miffin",
+    author="Andrea Correia",
+    packages=find_packages(),
+)
