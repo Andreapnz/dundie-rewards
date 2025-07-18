37907dadbffd5a9f72be8d785407790417245782
Andréa Correia <andrea_pnz@hotmail.com>
2025-07-18 14:52:17 -0300

    Salvando alterações antes de trocar de branch

--- tests/test_load.py
+++ tests/test_load.py
@@ -2,9 +2,17 @@ import pytest
 from dundie.core import load
 from .constants import PEOPLE_FILE
 
+
 @pytest.mark.unit
 @pytest.mark.high
-def test_load():
-    """Test function load function."""
-    assert len(load(PEOPLE_FILE)) == 2
+def test_load_positive_has_2_people(request):
+    """Test function load function.""" 
+    assert len(load(PEOPLE_FILE)) == 3
+
+@pytest.mark.unit
+@pytest.mark.high
+def test_load_positive_firts_name_starts_with_j(request):
+    """Test function load function""" 
     assert load(PEOPLE_FILE)[0][0] == "J"
+
+
